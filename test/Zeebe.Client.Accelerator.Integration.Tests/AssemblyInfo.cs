6c63f7d165d688a7058929c88def6937fc02f85b
Gunnar von der Beck <Gunnar.von-der-Beck@accso.de>
2026-03-18 11:29:08 +0100

    test: DisableTestParallelization for integration tests

---
+++ test/Zeebe.Client.Accelerator.Integration.Tests/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+namespace Zeebe.Client.Accelerator.Integration.Tests
+{
+    internal class AssemblyInfo
+    {
+    }
+}
